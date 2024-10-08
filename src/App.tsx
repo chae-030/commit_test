45d1139ee21034b6a17fcb7da83a9edd09ceeba9
ychae1997 <77264092+ychae1997@users.noreply.github.com>
2024-10-08 15:45:01 +0900

    Feat: 라우팅 및 레이아웃

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./components/common/Layout";
+import Survey from "./pages/Survey";
+import Container from "./components/common/Container";
+import Home from "./pages/Home";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Container children={<Home />} />} />
+        <Route element={<Layout />}>
+          <Route path="/survey" element={<Survey />} />
+          <Route path="/job" />
+          <Route path="/job/:position" />
+          <Route path="/result/:position" />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
