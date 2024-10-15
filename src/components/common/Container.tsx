f9b4db6bf53f791c71363c6fb08759930135c893
ychae030 <ychae1997@gmail.com>
2024-10-15 20:05:55 +0900

    design: common container pb 추가

--- src/components/common/Container.tsx
+++ src/components/common/Container.tsx
@@ -4,7 +4,7 @@ type ContainerProps = {
   children: React.ReactNode;
 };
 const Container = ({ children }: ContainerProps) => {
-  return <main className="max-w-lg w-11/12 mx-auto">{children}</main>;
+  return <main className="max-w-lg w-11/12 mx-auto pb-20">{children}</main>;
 };
 
 export default Container;
