045eceeba58000741d4a174ef2268b5dd250a666
sh <dygks0218@naver.com>
2024-10-22 14:52:59 +0900

    수현 tailwind 적용 (transition 제외)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
   theme: {
-    extend: { colors: { brand: "#FFC801", shadow: "rgba(0, 0, 0, 0.5)" } },
+    extend: {
+      colors: { brand: '#FFC801', shadow: 'rgba(0, 0, 0, 0.5)' },
+    },
     fontFamily: {
       // FontFamily 추가
       KELSI: [`"Kelsi 1", sans-serif`],
-      GMARKET: ["GmarketSansMedium"],
+      GMARKET: ['GmarketSansMedium'],
+    },
+    boxShadow: {
+      // BoxShadow 추가
+      jobShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
     },
   },
   plugins: [],
