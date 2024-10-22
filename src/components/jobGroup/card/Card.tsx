0e5756f8c27ab67ee9d9f03027acec277472ad70
sh <dygks0218@naver.com>
2024-10-22 14:52:59 +0900

    수현 tailwind 적용 (transition 제외)

--- src/components/jobGroup/card/Card.tsx
+++ src/components/jobGroup/card/Card.tsx
@@ -6,11 +6,14 @@ export type JobCardProps = {
 
 const JobCard = ({ title, description, imgUrl }: JobCardProps) => {
   return (
-    <div className="job-card">
-      <img src={imgUrl} alt={title} className="job-icon" />
+    <div
+      className="flex w-full h-48 text-left items-center p-5 mb-5
+    bg-white border border-brand rounded-xl shadow-jobShadow transition-width"
+    >
+      <img src={imgUrl} alt={title} className="w-64 h-44 mr-5" />
       <div className="job-text">
-        <h3 className="job-title">{title}</h3>
-        <p>{description}</p>
+        <h3 className="text-[#ffc801] font-bold text-xl pb-3">{title}</h3>
+        <p className="text-[#64748b] text-base">{description}</p>
       </div>
     </div>
   );
