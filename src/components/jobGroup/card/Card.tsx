9267d67f4240a993147ea9ac80657edda2895e93
ychae030 <ychae1997@gmail.com>
2024-10-26 23:22:39 +0900

    Design: 이미지 변경 및 너비 맞춤

--- src/components/jobGroup/card/Card.tsx
+++ src/components/jobGroup/card/Card.tsx
@@ -1,17 +1,43 @@
+import { Position } from "../../../types/constants";
+import frontImg from "../../../images/front.jpg";
+import backImg from "../../../images/back.jpg";
+import uiuxImg from "../../../images/uiux.jpg";
+import productImg from "../../../images/product.jpg";
+import projectImg from "../../../images/project.jpg";
+import qaImg from "../../../images/qa.jpg";
+import devopsImg from "../../../images/devops.jpg";
+
 export type JobCardProps = {
   title: string;
   description: string;
   imgUrl: string;
+  position: Position;
 };
 
-const JobCard = ({ title, description, imgUrl }: JobCardProps) => {
+const imageMap: Record<Position, string> = {
+  front: frontImg,
+  back: backImg,
+  uiux: uiuxImg,
+  product: productImg,
+  project: projectImg,
+  qa: qaImg,
+  devops: devopsImg,
+};
+
+const JobCard = ({ title, description, position }: JobCardProps) => {
   return (
     <div
-      className="flex w-full h-48 text-left items-center p-5 mb-5
+      className="flex w-full text-left items-center p-5 mb-5 gap-3 justify-between
     bg-white border border-brand rounded-xl shadow-jobShadow transition-width"
     >
-      <img src={imgUrl} alt={title} className="w-64 h-44 mr-5" />
-      <div className="job-text">
+      <div className="flex-shrink overflow-hidden w-5/12 h-44">
+        <img
+          className="w-full h-full object-cover"
+          src={imageMap[position]}
+          alt={title}
+        />
+      </div>
+      <div className="job-text w-7/12">
         <h3 className="text-[#ffc801] font-bold text-xl pb-3">{title}</h3>
         <p className="text-[#64748b] text-base">{description}</p>
       </div>
